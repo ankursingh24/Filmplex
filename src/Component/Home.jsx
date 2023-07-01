a16ac4e6b52d8c7f17a4938fb8921e2159d5ad4c
Ankur Singh <yankursingh254@gmail.com>
2023-07-01 15:05:47 +0530

    introduction and setup

---
+++ src/Component/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+
+const Home = () => {
+  return (
+
+    <div>
+ <CssBaseline/>
+        <h1>Home</h1></div>
+  )
+}
+
+export default Home
